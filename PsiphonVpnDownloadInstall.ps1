9d016ba96632469fd63722fb733080c961ddd89a
Vikram Chowdhury <vikram@themetromaxgroup.com>
2023-05-04 18:50:35 +0530

    working!!

--- PsiphonVpnDownloadInstall.ps1
+++ PsiphonVpnDownloadInstall.ps1
@@ -1,6 +1,11 @@
-$url = "https://psiphon.ca/psiphon3.exe"
-$output = "C:\Users\Vikram Chowdhury\Desktop\PowershellScript\Download\psiphon3.exe"
+$BaseDir = Get-Location
 
+$url = "https://psiphon.ca/psiphon3.exe"
+$filename = "Psiphon3.exe"
+
+$output = Join-Path -Path $BaseDir -ChildPath "\Download\$filename"
+
+Write-Host $output
 # Using BitsTransfer module to downlaod
 Import-Module BitsTransfer
 
@@ -9,4 +14,4 @@ Start-BitsTransfer -Source $url -Destination $output
 
 
 Write-Host "Starting to install the software"
-Start-Process "C:\Users\Vikram Chowdhury\Desktop\PowershellScript\Download\psiphon3.exe" -ArgumentList "/S /v/qn"
+Start-Process "$output" -ArgumentList "/S /v/qn"
